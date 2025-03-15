5bea380a3642e72fcbc705925b1a00de476282b5
Juan Sebastian Gutierrez <juan.gutierrez02@unicatolica.edu.co>
2025-03-14 22:55:10 -0500

    ajuste de Resta.py

--- calculadora-alterna/resta.py
+++ calculadora-alterna/resta.py
@@ -1,7 +1,11 @@
 from suma import sumar
 
 def restar(a, b):
-    c = 0
-    while a != b:
-        c =+ 1
-        a =- 1
+    if a >= b:
+        c = 0
+        while a != b:
+            c += 1  # Incrementa el contador
+            a -= 1  # Decrementa a hasta que sea igual a b
+        return c
+    else:
+        return "La resta no es posible ya que el primer número es menor al segundo número."
